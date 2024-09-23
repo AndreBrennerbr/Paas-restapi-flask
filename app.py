b286a67f39f1fc6a8fd8ed99e887ad15d61fb3e5
Andre Brenner <andre.luizrb@hotmail.com>
2024-09-23 00:28:47 -0300

    feat: iniciando projeto restapi flask

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+class Users(Resource):
+    def get(self):
+        return {"message": "User_1"}
+
+class User(Resource):
+    
+    def post(self):
+        return {"message":"teste post"}
+    
+    def get(self,cpf):
+        return{"message":cpf}
+  
+
+api.add_resource(Users, '/users')
+api.add_resource(User, '/user/<string:cpf>','/user')
+
+if __name__ == '__main__':
+    app.run(debug=True)
